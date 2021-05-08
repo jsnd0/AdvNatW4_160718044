4e3e09c188e063654edc98cefca237f5d23c03c9
Zero <jason.daryolla2@gmail.com>
2021-05-08 18:52:38 +0700

    set data binding

--- app/src/main/java/com/example/advweek4/view/StudentListAdapter.kt
+++ app/src/main/java/com/example/advweek4/view/StudentListAdapter.kt
@@ -3,17 +3,19 @@ package com.example.advweek4.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.advweek4.R
+import com.example.advweek4.databinding.StudentListItemBinding
 import com.example.advweek4.model.Student
 import kotlinx.android.synthetic.main.student_list_item.view.*
 import util.loadImage
 
-class StudentListAdapter (val studentList:ArrayList<Student>): RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>(){
-    class StudentViewHolder(val view:View):RecyclerView.ViewHolder(view)
+class StudentListAdapter (val studentList:ArrayList<Student>): RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
+    class StudentViewHolder(val view: StudentListItemBinding) : RecyclerView.ViewHolder(view.root)
 
-    fun updateStudentList(newStudentList:List<Student>){
+    fun updateStudentList(newStudentList: List<Student>) {
         studentList.clear()
         studentList.addAll(newStudentList)
         notifyDataSetChanged()
@@ -21,7 +23,13 @@ class StudentListAdapter (val studentList:ArrayList<Student>): RecyclerView.Adap
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val v = inflater.inflate(R.layout.student_list_item, parent, false)
+        //val v = inflater.inflate(R.layout.student_list_item, parent, false)
+        val v = DataBindingUtil.inflate<StudentListItemBinding>(
+            inflater,
+            R.layout.student_list_item,
+            parent,
+            false
+        )
         return StudentViewHolder(v)
     }
 
@@ -30,14 +38,15 @@ class StudentListAdapter (val studentList:ArrayList<Student>): RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-        holder.view.txtId.text = studentList[position].id
-        holder.view.txtName.text = studentList[position].name
-        holder.view.imageView.loadImage(studentList[position].photoUrl, holder.view.progressBar)
-
-
-        holder.view.btnDetail.setOnClickListener {
-            val action = StudentListFragmentDirections.actionStudentDetail(studentList[position].id.toString())
-            Navigation.findNavController(it).navigate(action)
-        }
+        holder.view.student = studentList[position]
+//        holder.view.txtId.text = studentList[position].id
+//        holder.view.txtName.text = studentList[position].name
+//        holder.view.imageView.loadImage(studentList[position].photoUrl, holder.view.progressBar)
+//
+//
+//        holder.view.btnDetail.setOnClickListener {
+//            val action = StudentListFragmentDirections.actionStudentDetail(studentList[position].id.toString())
+//            Navigation.findNavController(it).navigate(action)
+//        }
     }
 }
