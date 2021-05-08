c2eabf8a731341a21dfd134c13c6c19123562858
Zero <jason.daryolla2@gmail.com>
2021-05-08 20:35:34 +0700

    finished week 11 homework

--- app/src/main/java/com/example/advweek4/view/Interfaces.kt
+++ app/src/main/java/com/example/advweek4/view/Interfaces.kt
@@ -5,3 +5,11 @@ import android.view.View
 interface ButtonDetailCLickListener {
     fun onButtonDetailCLick(v: View)
 }
+
+interface ButtonNotifyClickListener{
+    fun onButtonNotifyClick(v:View)
+}
+
+interface ButtonUpdateClickListener{
+    fun onButtonUpdateClick(v:View)
+}
--- app/src/main/java/com/example/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/advweek4/view/StudentDetailFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.advweek4.R
+import com.example.advweek4.databinding.FragmentStudentDetailBinding
 import com.example.advweek4.viewmodel.DetailViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
@@ -20,15 +22,17 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 
 
-class StudentDetailFragment : Fragment() {
+class StudentDetailFragment : Fragment(),ButtonNotifyClickListener, ButtonUpdateClickListener {
     private lateinit var viewModel: DetailViewModel
+    private lateinit var dataBinding: FragmentStudentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_student_detail, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,33 +40,50 @@ class StudentDetailFragment : Fragment() {
         val studentId = StudentDetailFragmentArgs.fromBundle(requireArguments()).studentId
         viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
         viewModel.fetch(studentId)
-
+        dataBinding.notifylistener = this
+        dataBinding.updatelistener = this
         observeViewModel()
     }
 
     fun observeViewModel() {
         viewModel.studentLD.observe(
             viewLifecycleOwner,
-            Observer {
-                txtId.setText(viewModel.studentLD.value?.id.toString())
-                txtName.setText(viewModel.studentLD.value?.name.toString())
-                txtDob.setText(viewModel.studentLD.value?.dob.toString())
-                txtPhone.setText(viewModel.studentLD.value?.phone.toString())
-                imageView2.loadImage(viewModel.studentLD.value?.photoUrl, progressBarD)
+            Observer { dataBinding.student= it}
+//                txtId.setText(viewModel.studentLD.value?.id.toString())
+//                txtName.setText(viewModel.studentLD.value?.name.toString())
+//                txtDob.setText(viewModel.studentLD.value?.dob.toString())
+//                txtPhone.setText(viewModel.studentLD.value?.phone.toString())
+//                imageView2.loadImage(viewModel.studentLD.value?.photoUrl, progressBarD)
+//
+//                var student = it
+//                btnNotif.setOnClickListener {
+//                    Observable.timer(5, TimeUnit.SECONDS)
+//                        .subscribeOn(Schedulers.io())
+//                        .observeOn(AndroidSchedulers.mainThread())
+//                        .subscribe() {
+//                            MainActivity.showNotification(
+//                                student.name.toString(),
+//                                "A new notification created",
+//                                R.drawable.ic_baseline_person_24
+//                            )
+//                        }
+//                }
+//            })
+        )}
 
-                var student = it
-                btnNotif.setOnClickListener {
-                    Observable.timer(5, TimeUnit.SECONDS)
+    override fun onButtonNotifyClick(v: View) {
+        Observable.timer(5, TimeUnit.SECONDS)
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe() {
                             MainActivity.showNotification(
-                                student.name.toString(),
+                                v.tag.toString(),
                                 "A new notification created",
-                                R.drawable.ic_baseline_person_24
-                            )
+                                R.drawable.ic_baseline_person_24)
                         }
-                }
-            })
+    }
+
+    override fun onButtonUpdateClick(v: View) {
+        TODO("Not yet implemented")
     }
 }
